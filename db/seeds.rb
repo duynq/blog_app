3fd4a63c38930274e8cd143870a9023ee26f92ab
Duy Nguyen <quangduyx188@gmail.com>
2015-10-11 07:24:51 +0700

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Nguyen Duy",
+             email: "quangduyx188@gmail.com",
+             password:              "111111",
+             password_confirmation: "111111",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
