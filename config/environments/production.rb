acc64574950e1d655e6992e6124ad70f687cb40e
Duy Nguyen <quangduyx188@gmail.com>
2015-10-11 08:16:11 +0700

    chuong 10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,18 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'still-beach-2312.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'quangduyx188@gmail.com',
+    password:             'Nguyenduy93',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
